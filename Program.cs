879c467e704f51157ca027d358d07857ccf5b1fa
Sarah N <sarahniederits@gmail.com>
2024-11-05 13:46:40 +0100

    Git Init + CoutFinirCloture

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Terrain_;
+using System;
+
+namespace ExempleTerrain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Terrain UnTerrain = new Terrain("11 Rue des Chartreux, 69001 Lyon", 58f, 4, false);
+            Terrain UnAutreTerrain = new Terrain("138 Rue de Bordeaux, 16000 Angoulême", 3385.95f, 4, true);
+            Terrain UnDernierTerrain = new Terrain("26 Boulevard Claude Lorrin, 40100 Dax", 25.2f, 2, false);
+
+            Terrain[] CatalogueMaisons = new Terrain[] { UnTerrain, UnAutreTerrain, UnDernierTerrain };
+
+            foreach (Terrain M in CatalogueMaisons)
+            {
+                Console.WriteLine(M);
+            }
+        }
+    }
+}
---
+++ Terrain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Terrain_
+{
+    public class Terrain
+    {
+        public string Adresse;
+        public float Superficie;
+        public int NbCotesClotures;
+        public bool Riviere;
+
+        public Terrain(string adresse, float superficie, int nbCotesClotures, bool riviere)
+        {
+            Adresse = adresse;
+            Superficie = superficie;
+            NbCotesClotures = nbCotesClotures;
+            Riviere = riviere;
+        }
+
+        public override string ToString()
+        {
+            string toString = String.Format("Adresse = {0}\n", this.Adresse);
+            toString += String.Format("Superficie = {0}m²\n", this.Superficie);
+            toString += String.Format("Nombre de côtés cloturés = {0}\n", this.NbCotesClotures);
+            toString += String.Format("Présence d'une rivière = {0}\n", this.Riviere ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$", this.EvaluationValeur());
+            return toString;
+        }
+
+        public float EvaluationValeur()
+        {
+            int facteur = 3000;
+            if (this.Riviere) { facteur += 500; }
+            if (Regex.IsMatch(this.Adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.Adresse, @"\bLyon\b")) { facteur += 2000; }
+            return (this.Superficie * facteur) - CoutFinirCloture();
+        }
+
+        public int CoutFinirCloture()
+        {
+            int cout = 0;
+            if (this.NbCotesClotures < 3) { cout += 100 * (3 - NbCotesClotures); }
+            return cout;
+        }
+    }
+}
